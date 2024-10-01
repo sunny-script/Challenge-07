3498f3ac199f87aa67878b8690cbb220bbd29195
parkernichelle <parkernichelle@gmail.com>
2024-09-30 22:43:16 -0600

    added license badges and table of contents

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const questions = [
 
 // console.log(questions); Used to test if questions array was created correctly
 
-// TODO: Create a function to write README file
+// Function to write README file
 // Source: Xpert Learning Assistant
 function writeToFile(projectTitle, data) {
     const fileName = `${projectTitle.replace(/\s+/g, '_').toLowerCase()}_README.md`;
@@ -71,30 +71,82 @@ function writeToFile(projectTitle, data) {
     });
 }
 
-// TODO: Create a function to initialize app
+// TODO: Function to initialize app
 // Source: Xpert Learning Assistant
 function init() {
     inquirer.prompt(questions)
         .then((data) => {
-            const readmeContent = `
-            # ${data.projectTitle}
-            ## Description
-            ${data.description}
-            ## Installation Instructions
-            ${data.installationInstructions}
-            ## Usage Information
-            ${data.usageInformation}
-            ## Contribution Guidelines
-            ${data.contributionGuidelines}
-            ## Test Instructions
-            ${data.testInstructions}
-            ## License
-            ${data.license}
-            ## GitHub Username
-            ${data.githubUsername}
-            ## Email Address
-            ${data.email}
-            `;
+
+            // License badges mapped to corresponding URLs
+            // Source: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+            const licenseBadges = {
+                MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+                Apache: '[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+                GPL: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+                BSD: '[![License: BSD](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+                None: ''
+            };
+
+            // License confirmation mapped to corresponding license
+            // Source: my brain
+            const licenseConfirmation = {
+                MIT: 'This project is licensed under the MIT license.',
+                Apache: 'This project is licensed under the Apache license.',
+                GPL: 'This project is licensed under the GPL license.',
+                BSD: 'This project is licensed under the BSD license.',
+                None: 'This project is not licensed.'
+            };
+
+            const licenseBadge = licenseBadges[data.license];
+            const confirmationMessage = licenseConfirmation[data.license];
+
+            const readmeContent = 
+`
+# ${data.projectTitle} 
+${licenseBadge}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+${data.description}
+
+## Installation Instructions
+
+${data.installationInstructions}
+
+## Usage Information
+
+${data.usageInformation}
+
+## Contribution Guidelines
+
+${data.contributionGuidelines}
+
+## Test Instructions
+
+${data.testInstructions}
+
+## License
+
+${data.license}
+${licenseBadge}
+${confirmationMessage}
+
+## Questions
+
+Check out my [GitHub profile](https://github.com/${data.githubUsername}) 
+If you have any questions, you can reach me at: ${data.email}
+`;
+
             writeToFile(data.projectTitle, readmeContent);
         })
         .catch((error) => {
---
+++ parker_README.md
@@ -0,0 +1,43 @@
+# parker 
+[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+lsjdf
+
+## Installation Instructions
+
+lsdjf
+
+## Usage Information
+
+lsdkf
+
+## Contribution Guidelines
+
+lsjdf
+
+## Test Instructions
+
+lskdjf
+
+## License
+
+Apache
+[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+This project is licensed under the Apache license.
+
+## Questions
+
+Check out my [GitHub profile](https://github.com/sunny-script) 
+If you have any questions, you can reach me at: parkernichelle@gmail.com
